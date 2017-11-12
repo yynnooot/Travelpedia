3ae39e516eb3a4d62445b8f296ab2f6ea4da560d
Tony Yoon <tooonnyy@gmail.com>
2017-11-12 16:03:20 -0500

    logging description successful

--- client/Main.js
+++ client/Main.js
@@ -1,15 +1,8 @@
 import React from 'react'
-//import { API_KEY } from '../secret';
+import { API_KEY } from '../secret';
 import axios from 'axios'
 
-
-// handle file isnt linked async
-//
-// on submit callls uploadfiles
-// upload calls processfile
-// processfiles calls sendsFileTOClodu
-// send to cloud calls an ajax request
-var CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDXwcoDfCkDCScAfKYAYQo3lFL4-3h1jy0'
+var CV_URL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`
 
 
 export default class Main extends React.Component {
@@ -27,38 +20,43 @@ export default class Main extends React.Component {
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleFiles = this.handleFiles.bind(this)
-    this.handleClick = this.handleClick.bind(this)
+    // this.handleClick = this.handleClick.bind(this)
     this.uploadFile = this.uploadFile.bind(this)
     this.processFile = this.processFile.bind(this)
   }
-
-  handleClick() {
-    console.log('CLICKED')
-     axios.post('/api/cloud')
-      .then(res => {
-        this.setState({
-          data: res.data
-        })
+  
+  // *** <input onChange={this.handleFiles} type="file" name="fileField"/> ***//
+  handleFiles(evt) {
+    const reader = new FileReader();
+    const file = evt.target.files[0];
+    if (file) {
+      this.setState ({
+        fileName: file.name,
+        fileType: file.type,
+        file: file,
+        reader: reader
       })
+    }
+  }
+  
+  handleSubmit(evt) {
+    evt.preventDefault()
+    // console.log('SUBMIT')
+    this.uploadFile()
   }
 
   uploadFile() {
     let reader = new FileReader()
     reader.onloadend = this.processFile
-    // console.log('UPLOADFILE before readAsData:',this.state.file)
-    
     reader.readAsDataURL(this.state.file)
-    // console.log('UPLOADFILE after readAsData:',this.state.file)
   }
 
   processFile(evt) {
     var content = evt.target.result
     // function to convert
     const content64 = content.replace('data:image/jpeg;base64,', '')
-    // this.setState({
-    //   content: content64
-    // })
-    console.log('before axious state content',content64)
+    
+    //console.log('before axious state content',content64)
     const request = {
       requests: [
         {
@@ -77,56 +75,14 @@ export default class Main extends React.Component {
     axios.post(CV_URL, request)
       .then(res => {
         
-        console.log('res',res)
+        console.log('res',res.data.responses[0].landmarkAnnotations[0].description)
       })
       .catch(err => {
         console.log('err:', err)
       })
     }
     
-  handleFiles(evt) {
-    const reader = new FileReader();
-    const file = evt.target.files[0];
-    if (file) {
-      this.setState ({
-        fileName: file.name,
-        fileType: file.type,
-        file: file,
-        reader: reader
-      })
-    }
-    // console.log('HANDLEFILES:',this.state)
-  }
 
-  handleSubmit(evt) {
-    evt.preventDefault()
-    // console.log('SUBMIT')
-    this.uploadFile()
-
-    console.log('HANDLESUBMIT: ', this.state.content);
-  //   axios.post(CV_URL, {
-  //     requests: [
-  //       {
-  //         image: {
-  //           content: this.state.content
-  //         },
-  //         features: [
-  //           {
-  //             type: "LANDMARK_DETECTION",
-  //             maxResults: 1
-  //           }
-  //         ]
-  //       }
-  //     ]
-
-  //   })
-  //     .then(res => {
-  //       console.log(res)
-  //     })
-  //     .catch(err => {
-  //       console.log('err:', err)
-  //     })
-  }
 
   render() {
     console.log('RENDER:this.state: ', this.state);
@@ -141,7 +97,7 @@ export default class Main extends React.Component {
           <br />
           <button type="submit">CLICK ME</button>
         </form>
-        <button onClick={this.handleClick}>GOOGLE PING</button>
+        {/* <button onClick={this.handleClick}>GOOGLE PING</button> */}
       </div>
     )
   }
